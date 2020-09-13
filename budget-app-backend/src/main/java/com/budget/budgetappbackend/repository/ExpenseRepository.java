78b06ff11efd2c6ea3c0a8cecace426e9bee2e0a
Bartlomiej Calko <bartlomiej.calko@gmail.com>
2020-09-13 21:41:27 +0200

    backend - repository, added ExpenseSearchCriteria

---
+++ budget-app-backend/src/main/java/com/budget/budgetappbackend/repository/ExpenseRepository.java
@@ -0,0 +1,19 @@
+package com.budget.budgetappbackend.repository;
+
+import com.budget.budgetappbackend.model.Expense;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ExpenseRepository {
+
+    Optional<Expense> findById(Long id);
+
+    Expense save(Expense expense);
+
+    List<Expense> findAll();
+
+
+}
--- budget-app-backend/src/main/java/com/budget/budgetappbackend/service/ExpensesService.java
+++ budget-app-backend/src/main/java/com/budget/budgetappbackend/service/ExpensesService.java
@@ -1,4 +1,19 @@
 package com.budget.budgetappbackend.service;
 
+import com.budget.budgetappbackend.model.Expense;
+import com.budget.budgetappbackend.model.ExpensesSearchCriteria;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ExpensesService {
+
+    public Optional<List<Expense>> getExpenseBySearchCriteria(ExpensesSearchCriteria criteria);
+
+    public Expense createExpense(Expense expense);
+
+    Optional<List<Expense>> getAllExpenses();
+
+    public void deleteExpense(Long expenseId);
+
 }
